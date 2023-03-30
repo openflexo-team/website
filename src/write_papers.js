070c20632e0fd7420bdcbfbde825075b55c798c2
Ihab Benamer <benamerihab25@gmail.com>
2023-03-30 11:02:16 +0200

    MEDIUM  / update  papers layout

--- src/write_papers.js
+++ src/write_papers.js
@@ -11,17 +11,9 @@ const directoryPath = 'publis/';
 fs.readdir(directoryPath, function (err, files) {
 
     files.forEach(function (file) {
-        const filePath = path.join(directoryPath, file)
-
-        // fs.readFile(filePath, function(err, data) {
-        //     content += data.toString()
-        //     // console.log(content)
-        // });
-
-        var buffer = fs.readFileSync(filePath)
-        // console.log(buffer.toString())
-        content += buffer.toString()
+        const filePath  = path.join(directoryPath, file)
+        var buffer      = fs.readFileSync(filePath)
+        content         += buffer.toString()
     })
     fs.writeFileSync('data/papers.js', start + content + end)
-
 });
