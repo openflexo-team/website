a9eea0cfd913e2320359a6121cd5324732a702e7
Ihab Benamer <benamerihab25@gmail.com>
2023-10-18 11:50:07 +0200

    MEDIUM / update website sliders

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -13,10 +13,10 @@ function Banner( props) {
   return (
     <div className={styles.containerStyle}>
       <img className={styles.imgStyle} src={ props.img }/>
-      <div className={styles.textWrapperStyle} >
+      {/* <div className={styles.textWrapperStyle} >
         <h1 className={styles.h1Style}>{ props.title  }</h1>
         <h3 className={styles.h3Style}>{ props.header }</h3> 
-      </div>
+      </div> */}
     </div>
   );
 }
@@ -30,7 +30,7 @@ export default function SimpleSlider() {
       slidesToScroll: 1,
       arrows: false,
       autoplay: true,
-      autoplaySpeed: 5000,
+      autoplaySpeed: 0,
     };
 
     return (
