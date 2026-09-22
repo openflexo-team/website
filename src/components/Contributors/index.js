9e9bd5bf6c641ae5bc95d2d27a03b807ac4a027b
Sylvain Guérin <sylvain.guerin@imt-atlantique.fr>
2026-09-22 16:44:49 +0200

    MEDIUM / People page

--- src/components/Contributors/index.js
+++ src/components/Contributors/index.js
@@ -3,15 +3,29 @@ import clsx from 'clsx';
 import styles from './index.module.css';
 import ContributorList from '../../data/contributors.json';
 
-function Contributor({name, role, image, affiliation, email}){
+function initials(name) {
+    return name
+        .split(' ')
+        .filter(Boolean)
+        .map((part) => part[0])
+        .join('')
+        .toUpperCase();
+}
+
+function Contributor({name, role, image, affiliation, email, link}){
+    const displayName = link ? <a href={link} target="_blank" rel="noopener noreferrer">{name}</a> : name;
     return (
         <div className='col col--4'>
             <div className='avatar avatar--vertical margin-bottom--lg card'>
                 <div className={`text--center avatar__photo avatar__photo--xl margin-bottom--sm margin-top--lg ${styles.shadowSm}`} >
-                    <img src={image} alt={name}/>
+                    {image ? (
+                        <img src={image} alt={name}/>
+                    ) : (
+                        <div className={styles.placeholderAvatar}>{initials(name)}</div>
+                    )}
                 </div>
                 <div className="text--center padding-horiz--md margin-bottom--lg">
-                    <div className='avatar__name'>{name}</div>
+                    <div className='avatar__name'>{displayName}</div>
                     <small className='avatar__subtitle'>{role}</small>
                     {affiliation && <div className={styles.affiliation}>{affiliation}</div>}
                     {email && (
